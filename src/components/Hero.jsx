dd7edf7ea23371755795809fca7ad1ccac7fed3e
danny-stoya <yordan.y.stoyanov@gmail.com>
2023-06-07 20:03:30 +0300

    fix title size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,14 @@ import {Tech} from "./components.js";
 const Hero = () => {
     return (<>
             <div
-                className={`${styles.paddingX} absolute top-1/2 -translate-y-1/2 inset-0 z-10 max-w-7xl mx-auto flex flex-row  gap-5`}>
-                <div className="flex flex-col justify-center items-center mt-5">
+                className={`${styles.paddingX} absolute top-[70px] md:top-1/2 md:-translate-y-1/2 inset-0 z-10 max-w-7xl mx-auto flex flex-row  gap-5`}>
+                <div className="flex flex-col  items-center mt-5">
                     <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
                     <div className="w-1 sm:h-80 h-40 violet-gradient"></div>
                 </div>
                 <div>
                     <h1 className={`${styles.heroHeadText} sm:mb-5 text-white`}>Hi. I'm <span
-                        className="text-[#915eff]">Yordan 🚀</span></h1>
+                        className="text-[#915eff]">Yordan <span>🚀</span></span></h1>
 
                     <div className="mt-4 max-w-4xl">
                         <p className="text-secondary text-[15px] sm:text-[17px] leading-[30px]">
